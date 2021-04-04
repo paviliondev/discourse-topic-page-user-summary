e5ebbec5da73537f6380beeff5539b3ec48d5ff4
Faizaan Gagan <fzngagan@gmail.com>
2021-04-04 23:44:26 +0530

    FIX: show details for the topic OP user

--- plugin.rb
+++ plugin.rb
@@ -10,23 +10,19 @@ enabled_site_setting :topic_page_user_summary_enabled
 
 after_initialize do
   add_to_class(:topic_view, :user_summary) do
-    if topic_user.present?
-      user_summary = UserSummary.new(topic_user.user, guardian)
-      user_summary
-    else
-      false
-    end
+    user_summary = UserSummary.new(topic.user, guardian)
+    user_summary
   end
 
   add_to_serializer(:topic_view, :created_by) do
-    if has_topic_user? && object.user_summary.present?
-      UserSummarySerializer.new(object.user_summary, scope: object.guardian, root: false)
-    else
-      {}  
+    if scope.can_see_summary_stats?(object.topic.user)
+      UserSummarySerializer.new(object.user_summary, scope: scope, root: false)
     end
   end
 
   add_to_serializer(:user_summary, :created_at) do
-    object.user.created_at
+    if scope.can_see_summary_stats?(object.user)
+      object.user.created_at
+    end
   end
 end
